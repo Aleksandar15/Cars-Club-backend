72de33972494a57dfe2a7c3d25a9716c0bb1994d
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-06 00:00:05 +0200

    CORS setup

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import authRoutes from "./routes/authRoutes";
+import cors from "cors";
 
 const app: Application = express();
+
+// CORS configurations
+app.use(cors());
 
 // In case process.env.PORT is undefined use 3000
 const PORT: number = Number(process.env.PORT ?? 3000);
