3a9d5825d10d275a46e4053f129971e6b0762010
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-07 23:48:04 +0200

    Updated database SQL; added refresh_tokens table

--- database.sql
+++ database.sql
@@ -2,12 +2,33 @@ CREATE DATABASE cars_club;
 
 CREATE extension IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE users(
+CREATE TABLE users (
   user_id uuid DEFAULT uuid_generate_v4(),
-  user_name VARCHAR(255) NOT NULL,
-  user_email VARCHAR(255) NOT NULL UNIQUE,
-  user_password VARCHAR(255) NOT NULL,
-  refresh_tokens VARCHAR(1000)[],
+  user_name VARCHAR(30) NOT NULL,
+  user_email VARCHAR(50) NOT NULL UNIQUE,
+  user_password VARCHAR(100) NOT NULL,
+  -- refresh_tokens VARCHAR(1000)[],
   user_role VARCHAR(55) DEFAULT 'user',
+  user_created_at timestamptz DEFAULT current_timestamp,
   PRIMARY KEY (user_id)
 );
+
+CREATE TABLE refresh_tokens (
+  refresh_token_id uuid DEFAULT uuid_generate_v4(),
+  user_id uuid NOT NULL,
+  refresh_token VARCHAR(1000) NOT NULL,
+  PRIMARY KEY (refresh_token_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- CREATE TABLE posts (
+--   post_id uuid DEFAULT uuid_generate_v4(),
+--   post_title VARCHAR(100) NOT NULL,
+--   post_content VARCHAR(2000) NOT NULL,
+-- )
+
+-- CREATE TABLE comments ()
+
+-- TESTS
+-- Create test user:
+INSERT INTO users (user_name, user_email, user_password) VALUES ('test', 'test@test.com', 'password');
