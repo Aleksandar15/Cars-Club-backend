13e4b5b81b2d7af4fc400bb3892c80ccf4b425f6
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-07 18:11:42 +0200

    Added database SQL commands

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE cars_club;
+
+CREATE extension IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users(
+  user_id uuid DEFAULT uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (user_id)
+);
