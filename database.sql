fc4dc654aca448c884ab82a9deefc065cec2351d
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-07 22:03:56 +0200

    Updated database SQL

--- database.sql
+++ database.sql
@@ -7,5 +7,7 @@ CREATE TABLE users(
   user_name VARCHAR(255) NOT NULL,
   user_email VARCHAR(255) NOT NULL UNIQUE,
   user_password VARCHAR(255) NOT NULL,
+  refresh_tokens VARCHAR(1000)[],
+  user_role VARCHAR(55) DEFAULT 'user',
   PRIMARY KEY (user_id)
 );
