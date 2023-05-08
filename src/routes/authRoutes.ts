57baba0955d86e4129566cfca77b8bad6864cfe5
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-08 20:04:43 +0200

    Updated auth with middleware for input sanitization

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import loginController from "../controllers/auth/loginController";
 import registerController from "../controllers/auth/registerController";
-
 import refreshtokenController from "../controllers/auth/refreshTokenController";
+import validAuthInfo from "../middlewares/validAuthInfo";
 
 const router = Router();
 
-router.post("/login", loginController);
+router.post("/login", validAuthInfo, loginController);
 
-router.post("/register", registerController);
+router.post("/register", validAuthInfo, registerController);
 
 router.get("/refreshtoken", refreshtokenController);
 
