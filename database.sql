27124d4acb7e441c9fb99d6cce1ae818998caf70
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-08 17:27:01 +0200

    Updated database.sql with modified refresh_tokens table

--- database.sql
+++ database.sql
@@ -7,7 +7,6 @@ CREATE TABLE users (
   user_name VARCHAR(30) NOT NULL,
   user_email VARCHAR(50) NOT NULL UNIQUE,
   user_password VARCHAR(100) NOT NULL,
-  -- refresh_tokens VARCHAR(1000)[],
   user_role VARCHAR(55) DEFAULT 'user',
   user_created_at timestamptz DEFAULT current_timestamp,
   PRIMARY KEY (user_id)
@@ -32,3 +31,5 @@ CREATE TABLE refresh_tokens (
 -- TESTS
 -- Create test user:
 INSERT INTO users (user_name, user_email, user_password) VALUES ('test', 'test@test.com', 'password');
+-- Create test refresh_token:
+INSERT INTO refresh_tokens (user_id, refresh_token) VALUES ('change-this-into-real-uuid', 'abc123');
