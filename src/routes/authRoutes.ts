c3064591823ab5cad9d54c4fb18fcae12a195ed1
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-10 00:04:36 +0200

    Implemented logoutController

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,12 +3,15 @@ import loginController from "../controllers/auth/loginController";
 import registerController from "../controllers/auth/registerController";
 import refreshtokenController from "../controllers/auth/refreshTokenController";
 import validAuthInfo from "../middlewares/validAuthInfo";
+import logoutController from "../controllers/logout/logoutController";
 
 const router = Router();
 
 router.post("/login", validAuthInfo, loginController);
 
 router.post("/register", validAuthInfo, registerController);
+
+router.delete("/logout", logoutController);
 
 router.get("/refreshtoken", refreshtokenController);
 
