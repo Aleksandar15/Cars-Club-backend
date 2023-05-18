f9dc295aef8c7c68720ed179e23550591efa4d5e
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-18 03:22:09 +0200

    Updated CreatePostController

--- src/controllers/posts/createPostController.ts
+++ src/controllers/posts/createPostController.ts
@@ -13,22 +13,49 @@ const createPostController: RequestHandler = async (req, res) => {
       "req.file.buffer createPostController11111:",
       req?.file?.buffer
     );
-    const { rows: imageRows } = await database.query(
-      `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
-      [req?.file?.buffer]
+    // const { rows: imageRows } = await database.query(
+    //   `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
+    //   [req?.file?.buffer]
+    // );
+    // // const { rows: imageRows } = await database.query(
+    // //   `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
+    // //   [req?.body?.image]
+    // // );
+    // // console.log("req.body.binaryString:", req.body?.binaryString);
+    // // const imageBuffer = Buffer.from(req.body.binaryString, "binary");
+    // // const { rows: imageRows } = await database.query(
+    // //   `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
+    // //   [imageBuffer]
+    // // );
+
+    const {
+      post_title,
+      post_image,
+      post_description,
+      post_contact_number,
+      post_asking_price,
+      post_asking_price_currency,
+    } = req.body;
+
+    const { rows: postsRows } = await database.query(
+      `INSERT INTO posts (post_title, post_image, post_description, 
+        post_contact_number, post_asking_price, 
+        post_asking_price_currency)
+         VALUES ($1, $2, $3, $4, $5, $6)`,
+      [
+        post_title,
+        post_image,
+        post_description,
+        post_contact_number,
+        post_asking_price,
+        post_asking_price_currency,
+      ]
     );
-    // const { rows: imageRows } = await database.query(
-    //   `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
-    //   [req?.body?.image]
-    // );
-    // console.log("req.body.binaryString:", req.body?.binaryString);
-    // const imageBuffer = Buffer.from(req.body.binaryString, "binary");
-    // const { rows: imageRows } = await database.query(
-    //   `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
-    //   [imageBuffer]
-    // );
-    console.log("imageRows createPostController:", imageRows);
-    res.end("done");
+    console.log("postsRows createPostController:", postsRows);
+    // res.end("done");
+    return res
+      .status(200)
+      .json({ isSuccessful: true, message: "Post has been created" });
   } catch (err) {
     if (err instanceof Error) {
       console.log("err.message createPostController:", err?.message);
