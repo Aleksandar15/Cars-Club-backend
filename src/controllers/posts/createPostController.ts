a8e4a1d669f2bec93a4bf0e0bc22a163e4bd2531
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-18 03:58:31 +0200

    Code cleanup

--- src/controllers/posts/createPostController.ts
+++ src/controllers/posts/createPostController.ts
@@ -7,30 +7,8 @@ const createPostController: RequestHandler = async (
   res
 ) => {
   try {
-    // return res.status(200).json({ isSuccessful: true, message: "Received" });
-    console.log("req.body createPostController11111:", req.body);
-    console.log("req.body.image createPostController11111:", req.body.image);
-    console.count("HANGING");
-
-    console.log("req.file createPostController11111:", req.file);
-    console.log(
-      "req.file.buffer createPostController11111:",
-      req?.file?.buffer
-    );
-    // const { rows: imageRows } = await database.query(
-    //   `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
-    //   [req?.file?.buffer]
-    // );
-    // // const { rows: imageRows } = await database.query(
-    // //   `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
-    // //   [req?.body?.image]
-    // // );
-    // // console.log("req.body.binaryString:", req.body?.binaryString);
-    // // const imageBuffer = Buffer.from(req.body.binaryString, "binary");
-    // // const { rows: imageRows } = await database.query(
-    // //   `INSERT INTO posts (image) VALUES ($1) RETURNING *`,
-    // //   [imageBuffer]
-    // // );
+    console.log("req.body createPostController:", req.body);
+    console.log("req.file createPostController:", req?.file);
 
     const { title, image, description, contactNumber, askingPrice, currency } =
       req.body;
@@ -51,7 +29,6 @@ const createPostController: RequestHandler = async (
       ]
     );
     console.log("postsRows createPostController:", postsRows);
-    // res.end("done");
     return res
       .status(200)
       .json({ isSuccessful: true, message: "Post has been created" });
