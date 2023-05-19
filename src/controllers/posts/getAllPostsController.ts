5209bd2ab4545d7c75abe6d63b94db263dfb53f0
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-19 16:09:07 +0200

    Updated getAllPostsController to return ALL posts instead; removed its testing of 'LIMIT 3' querying

--- src/controllers/posts/getAllPostsController.ts
+++ src/controllers/posts/getAllPostsController.ts
@@ -6,55 +6,20 @@ const getAllPostsController: RequestHandler<{ id: string }> = async (
   res
 ) => {
   try {
-    console.log("req.params.id:", req.params.id);
-    // const gotImageById = await database.query(
+    // console.log("req.params.id:", req.params.id); // Not using for "ALL"
+    // const { rows: gotThreePostsROWS } = await database.query(
     //   `
-    // SELECT image FROM posts WHERE id=$1`,
-    //   [req.params.id]
-    // );
-    const { rows: gotThreePostsROWS } = await database.query(
+    // SELECT * FROM posts ORDER BY post_created_at DESC LIMIT 3`
+    // ); // LIMITing works, however the query was used for testing.
+    // console.log("gotThreePostsROWS getAllPostsController:", gotThreePostsROWS);
+    // return res.status(200).json({ gotThreePostsROWS });
+
+    const { rows: gotAllPostsROWS } = await database.query(
       `
-    SELECT * FROM posts ORDER BY post_created_at DESC LIMIT 3`
+    SELECT * FROM posts ORDER BY post_created_at DESC`
     );
-    console.log(
-      "~>~>~>~>~>gotThreePostsROWS getAllPostsController:",
-      gotThreePostsROWS
-    );
-    // const base64Image = Buffer.from(gotImageById?.rows[0].image).toString(
-    //   "base64"
-    // );
-    // headers are NOT needed
-    // res.setHeader(
-    //   "Content-Disposition",
-    //   `attachment; filename="${"mercedes.jpg"}"` // tests hardcoded
-    // );
-
-    // // Below works but I'll have to use the imgSrc myself in React.
-    // return res.status(200).json({ base64Image });
-
-    // // Instead pass the finished SRC's value
-    // const imageSrc = `data:image/jpeg;base64,${base64Image}`;
-    // return res.status(200).json({ imageSrc });
-
-    //
-    // return res.status(200).json({ imageSrc: gotImageById?.rows[0]?.image });
-
-    //
-    // const imageBuffer = gotImageById?.rows[0]?.image;
-    // const blob = new Blob([imageBuffer], { type: "image/jpeg" });
-    // const imageSrc = URL.createObjectURL(blob);
-    // // ERROR ON Frontend:
-    // // Not allowed to load local resource: blob:nodedata:9a2a5d27-65c7-4e60-bb20-3640fae3dda1
-    // return res.status(200).json({ imageSrc });
-
-    //
-    // const imageBuffer = gotImageById?.rows[0]?.image;
-    // const base64Image = imageBuffer.toString("base64");
-    // return res.status(200).json({ imageSrc: base64Image });
-    //
-
-    // return res.status(200).json({ imageSrc: gotImageById?.rows[0].image });
-    return res.status(200).json({ gotThreePostsROWS });
+    // console.log("gotAllPostsROWS getAllPostsController:", gotAllPostsROWS);
+    return res.status(200).json({ gotAllPostsROWS });
   } catch (err) {
     console.log("getAllPostsController ERR:", err);
   }
