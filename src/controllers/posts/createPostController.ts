711d89768e90afa97b9a416791619e632ed2896c
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-20 06:05:34 +0200

    Updated createPostController to INSERT VALUES INTO the new updated posts table's columns

--- src/controllers/posts/createPostController.ts
+++ src/controllers/posts/createPostController.ts
@@ -10,15 +10,25 @@ const createPostController: RequestHandler = async (
     console.log("req.body createPostController:", req.body);
     console.log("req.file createPostController:", req?.file);
 
-    const { title, description, contactNumber, askingPrice, currency } =
-      req.body;
+    const {
+      title,
+      description,
+      contactNumber,
+      askingPrice,
+      currency,
+      user_name,
+      user_email,
+    } = req.body;
 
     const { rows: postsRows } = await database.query(
       `INSERT INTO posts (post_title, post_image_buffer, 
         post_description, post_contact_number, 
         post_asking_price, post_asking_price_currency, 
-        user_id)
-         VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+        user_id,
+        post_created_by_user_name,
+        post_created_by_user_email,
+        post_created_by_user_role)
+         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
       [
         title,
         req.file?.buffer, //Multer.File object populated by single() middleware.
@@ -27,6 +37,9 @@ const createPostController: RequestHandler = async (
         askingPrice,
         currency,
         req.user_id, // Comes from authorizeJWT middleware
+        user_name,
+        user_email,
+        req.user_role, // also from JWT TOKEN
       ]
     );
     // console.log("postsRows createPostController:", postsRows);
