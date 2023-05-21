2317bc911d3c2107e01cb169ba75748eab7ac3ec
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-05-21 21:24:57 +0200

    Updated editPostController to handle IMAGES updates & finalized non-image-updates cases

--- src/controllers/posts/editPostController.ts
+++ src/controllers/posts/editPostController.ts
@@ -40,6 +40,9 @@ const editPostController: RequestHandler = async (
     // LOGIC continuation
     // The rest of PROPS should NOT be '' (EXCEPT req.body.image).
 
+    // NOTE that post_created_at won't modify however in the
+    // future I can add a column "post_last_editted_at".
+
     let isEmpty = false;
     for (const property in req.body) {
       if (
@@ -48,37 +51,92 @@ const editPostController: RequestHandler = async (
           req.body[property] === null) &&
         property !== "image"
       ) {
-        // if they're empty "" or UNDEFINED/NULL, set isEmpty=true;
+        // if they're empty "" or UNDEFINED/NULL, set isEmpty=true
         isEmpty = true;
         break;
       }
     }
 
-    // // Case: User doesn't wanna update the image, ONLY the fields.
-    // if (req.file === undefined && req.body.image === "") {
-    //   const {rows: edittedPostROWS} = await database.query(
-    //     `
-    //     UPDATE posts SET
-    //     post_title=$1, post_description=$2,
-    //     post_contact_number=$3,
-    //     post_asking_price=$4,
-    //     post_asking_price_currency=$5
-    //     WHERE
-    //     `,
-    //     [
-    //       req.body.title,
-    //       req.body.description,
-    //       req.body.contactNumber,
-    //       req.body.askingPrice,
-    //       req.body.currency,
-    //     ]
-    //   );
-    //   console.log('')
-    // }
-    return res.status(200).json({
-      isSuccessful: true,
-      message: "Post has been found",
-    });
+    // Extra security 'req.user_id' coming from authorizeJWT
+    if (req.user_id === req.params.user_id) {
+      // Case: User doesn't wanna update the image, ONLY the rest.
+      if (req.file === undefined && req.body.image === "") {
+        const { rows: edittedPostROWS } = await database.query(
+          `
+          UPDATE posts SET
+          post_title=$1, post_description=$2,
+          post_contact_number=$3,
+          post_asking_price=$4,
+          post_asking_price_currency=$5
+          WHERE post_id=$6 AND user_id=$7
+          RETURNING *
+          `,
+          [
+            req.body.title,
+            req.body.description,
+            req.body.contactNumber,
+            req.body.askingPrice,
+            req.body.currency,
+            req.params.post_id,
+            req.params.user_id,
+          ]
+        );
+        if (edittedPostROWS.length === 0) {
+          return res.status(400).json({
+            isSuccessful: false,
+            message: "Error - failed to edit post",
+          });
+        }
+        return res.status(200).json({
+          isSuccessful: true,
+          message: "Post has been editted",
+          // Frontend doesn't need the edittedPostROWS.
+        });
+      } else {
+        // req.file is NOT UNDEFINED
+        // -> User has provided a new image
+        // & wants to update image as well as other Fields.
+        const { rows: edittedPostROWS } = await database.query(
+          `
+          UPDATE posts SET
+          post_title=$1, post_description=$2,
+          post_contact_number=$3,
+          post_asking_price=$4,
+          post_asking_price_currency=$5,
+          post_image_buffer=$6
+          WHERE post_id=$7 AND user_id=$8
+          RETURNING *
+          `,
+          [
+            req.body.title,
+            req.body.description,
+            req.body.contactNumber,
+            req.body.askingPrice,
+            req.body.currency,
+            req.file?.buffer, // Coming from multerMiddleware
+            req.params.post_id,
+            req.params.user_id,
+          ]
+        );
+        if (edittedPostROWS.length === 0) {
+          return res.status(400).json({
+            isSuccessful: false,
+            message: "Error - failed to edit post",
+          });
+        }
+        return res.status(200).json({
+          isSuccessful: true,
+          message: "Post has been editted",
+          // Frontend doesn't need the edittedPostROWS.
+        });
+      }
+    } else {
+      // req.user_id doesn't match req.params_user_id
+      return res.status(403).json({
+        isSuccessful: false,
+        message: "Error - user_id doesn't match the token ID",
+      });
+    }
   } catch (err) {
     if (err instanceof Error) {
       console.log("err.message editPostController:", err?.message);
